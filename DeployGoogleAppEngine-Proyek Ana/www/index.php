4048bd850e1574f9bac2e988c4bedf0237218a39
anatsurayyaz <117384535+anatsurayyaz@users.noreply.github.com>
2023-06-01 02:16:44 +0700

    Add files via upload

---
+++ DeployGoogleAppEngine-Proyek Ana/www/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- link style  -->
+    <link rel="stylesheet" href="css/style.css" />
+    <title>profile Ana Tsurayya Zakiya</title>
+  </head>
+  <body>
+    <!-- header website dan navbar -->
+    <header>
+      <!-- navbar  -->
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#front-end">Front-End</a></li>
+          <li><a href="#ui-ux">UI/UX</a></li>
+          <li><a href="#cloud-computing">Cloud Computing</a></li>
+        </ul>
+      </nav>
+      <!-- header  -->
+      <div class="slider">
+        <div class="slide">
+          <img src="images/slide1.jpg" alt="Slide 1" />
+        </div>
+        <div class="slide">
+          <img src="images/slide2.jpg" alt="Slide 2" />
+        </div>
+        <div class="slide">
+          <img src="images/slide3.jpg" alt="Slide 3" />
+        </div>
+        <a class="prev" onclick="prevSlide()">&#10094;</a>
+        <a class="next" onclick="nextSlide()">&#10095;</a>
+      </div>
+    </header>
+
+    <!-- content  -->
+    <main>
+      <div id="content">
+        <article class="card" id="artikel">
+          <h2>Informatica Developer</h2>
+          <div class="menu-item">
+            <img src="images/front-end.jpg" alt="front-end" />
+            <h3><a href="#front-end">Front End Developer</a></h3>
+          </div>
+          <div class="menu-item">
+            <img src="images/ui-ux.jpg" alt="ui-ux" />
+            <h3><a href="#ui-ux">UI/UX Designer</a></h3>
+          </div>
+          <div class="menu-item">
+            <img src="images/cloud-computing.jpg" alt="js" />
+            <h3><a href="#cloud-computing">Cloud Computing Developer</a></h3>
+          </div>
+        </article>
+
+        <article class="card" id="front-end">
+          <h2>Front End Developer</h2>
+          <p>
+            Seorang Front End Developer adalah seorang profesional di bidang pengembangan perangkat lunak yang bertanggung jawab untuk merancang, mengembangkan, dan memelihara bagian depan (front end) suatu aplikasi atau website. Mereka fokus pada aspek pengalaman pengguna (user experience) dan interaksi antarmuka pengguna. <br>
+			<br> Tugas seorang Front End Developer meliputi: <br/>
+			1. Mengimplementasikan desain yang telah dibuat oleh UI/UX Designer ke dalam kode-kode web atau aplikasi. <br>
+			2. Mengembangkan fitur-fitur interaktif yang meningkatkan pengalaman pengguna, seperti animasi, validasi form, dan navigasi yang responsif.<br>
+			3. Menyelaraskan desain dan fungsionalitas antarmuka pengguna dengan memastikan tampilan dan perilaku yang konsisten di berbagai perangkat dan browser yang berbeda. <br>
+			4. Mengoptimalkan performa dan kecepatan loading halaman dengan menggunakan teknik-teknik pengkodean yang efisien. <br>
+			5. Menyelidiki dan memperbaiki bug atau masalah yang terjadi pada bagian depan aplikasi atau website. <br>
+			6. Berkolaborasi dengan tim pengembangan lain, seperti Backend Developer dan UI/UX Designer, untuk memastikan keselarasan dalam pengembangan produk. <br>
+			7. Mempelajari dan menerapkan teknologi dan tren terbaru dalam pengembangan web untuk meningkatkan kualitas dan kecanggihan produk. <br>
+			<br> Front End Developer biasanya memiliki pengetahuan mendalam tentang bahasa pemrograman web seperti HTML, CSS, dan JavaScript, serta kerangka kerja (framework) dan library terkait seperti React, Angular, atau Vue.js. Mereka juga harus memahami konsep desain antarmuka pengguna (UI) dan pengalaman pengguna (UX) untuk menciptakan antarmuka yang menarik dan mudah digunakan. <br/>
+          </p>
+          <section>
+            <h3>HTML (Hypertext Markup Language)</h3>
+            <img src="images/html.jpg" alt="HTML" class="content-image" />
+            <p>
+              HTML (Hypertext Markup Language) adalah bahasa markup yang digunakan untuk membuat struktur dan konten dari sebuah halaman web. HTML mengatur tampilan dan konten dari sebuah halaman web melalui tag atau elemen HTML. Setiap tag
+              HTML memiliki sintaks atau format yang khusus dan masing-masing memiliki fungsi dan tujuan yang berbeda. Beberapa contoh tag HTML yang sering digunakan antara lain:
+            </p>
+            <ul>
+              <li>&lt;html&gt;: tag pembuka untuk halaman web</li>
+              <li>&lt;head&gt; : tag yang berisi informasi tentang halaman web, seperti judul halaman, meta tag, dan script</li>
+              <li>&lt;body&gt; : tag yang berisi konten dari halaman web</li>
+              <li>&lt;h1&gt; hingga &lt;h6&gt; : tag untuk membuat judul atau heading</li>
+              <li>&lt;p&gt; : tag untuk membuat paragraf</li>
+              <li>&lt;a&gt; : tag untuk membuat hyperlink atau tautan</li>
+              <li>&lt;img&gt; : tag untuk menampilkan gambar</li>
+              <li>&lt;ul&gt; dan &lt;li&gt; : tag untuk membuat daftar dengan bullet point</li>
+              <li>&lt;table&gt; dan &lt;tr&gt; : tag untuk membuat tabel dan baris tabel</li>
+            </ul>
+            <p>
+              HTML biasanya ditulis dengan menggunakan text editor atau Integrated Development Environment (IDE), kemudian disimpan dengan ekstensi file .html. Setelah halaman web selesai dibuat, halaman tersebut dapat diakses melalui
+              browser web seperti Google Chrome, Mozilla Firefox, atau Microsoft Edge.
+            </p>
+          </section>
+          <section>
+            <h3>CSS (Cascading Style Sheets)</h3>
+            <p>
+              CSS (Cascading Style Sheets) adalah bahasa stylesheet yang digunakan untuk mengontrol tampilan dan tata letak dari dokumen HTML dan XML. Dengan CSS, Anda dapat mengubah warna teks, font, margin, padding, posisi, dan banyak
+              lagi dalam dokumen HTML dan XML. CSS menggunakan aturan-aturan untuk menghubungkan elemen HTML atau XML dengan gaya tertentu. Aturan CSS terdiri dari selektor dan deklarasi. Selektor adalah bagian dari aturan CSS yang
+              menentukan elemen HTML atau XML mana yang akan diberikan gaya. Deklarasi terdiri dari sifat dan nilai. Sifat adalah aspek yang akan diubah dalam elemen HTML atau XML, seperti warna teks atau margin, sedangkan nilai adalah
+              nilai yang diberikan pada sifat tersebut.
+            </p>
+          </section>
+          <section>
+            <h3>JavaScript</h3>
+            <img src="images/js.jpg" alt="JS" class="content-image" />
+            <p>
+              JavaScript adalah bahasa pemrograman tingkat tinggi yang digunakan untuk mengembangkan aplikasi web interaktif. JavaScript dapat digunakan untuk menambahkan interaksi dan fungsi ke halaman web seperti validasi form, animasi,
+              efek transisi, dan manipulasi DOM. JavaScript dapat dijalankan pada browser web atau pada server menggunakan platform seperti Node.js. Saat dijalankan di browser, JavaScript dapat diintegrasikan dengan HTML dan CSS untuk
+              membuat tampilan dan interaksi yang dinamis pada halaman web.
+            </p>
+          </section>
+        </article>
+
+        <article class="card" id="ui-ux">
+          <h2>UI/UX Designer</h2>
+		  <img src="images/ui-ux.jpg" alt="ui-ux" class="content-image" />
+          <p>
+            Seorang UI/UX Developer, atau disebut juga UI/UX Designer atau UI/UX Engineer, adalah seorang profesional di bidang desain antarmuka pengguna (UI) dan pengalaman pengguna (UX) yang bertanggung jawab untuk merancang dan mengembangkan pengalaman interaktif yang optimal bagi pengguna suatu produk atau layanan digital.
+			<br>Tugas seorang UI/UX Developer meliputi: <br />
+          </p>
+          <ul>
+            <li>Riset Pengguna: Melakukan penelitian dan menganalisis kebutuhan, preferensi, dan perilaku pengguna potensial untuk mendapatkan pemahaman yang mendalam tentang audiens target produk atau layanan digital.</li>
+            <li>Perancangan Antarmuka Pengguna (UI Design): Membuat desain visual antarmuka pengguna yang menarik dan estetis, termasuk pemilihan warna, tipografi, ikon, dan tata letak yang efektif. Desain ini harus memperhatikan prinsip desain UI dan gaya visual yang konsisten dengan merek atau produk yang bersangkutan.</li>
+            <li>Perancangan Pengalaman Pengguna (UX Design): Merancang alur kerja (workflow) dan struktur navigasi yang intuitif untuk memastikan pengalaman pengguna yang lancar dan mudah digunakan. Ini melibatkan membuat wireframe, storyboard, dan prototipe interaktif yang menggambarkan alur interaksi pengguna dengan produk.</li>
+            <li>Pengembangan Antarmuka Pengguna (UI Development): Mengimplementasikan desain antarmuka pengguna ke dalam kode-kode web atau aplikasi. Biasanya, seorang UI/UX Developer memiliki pengetahuan dan keahlian dalam teknologi front-end seperti HTML, CSS, dan JavaScript, serta mampu menggunakan alat-alat desain dan pengembangan terkait seperti Adobe XD, Sketch, Figma, atau InVision.</li>
+			<li>Pengujian dan Iterasi: Melakukan pengujian pengguna (user testing) untuk mengumpulkan umpan balik dan melihat bagaimana pengguna berinteraksi dengan produk. Berdasarkan hasil pengujian, melakukan iterasi dan perbaikan desain untuk meningkatkan pengalaman pengguna.</li>
+			<li>Kolaborasi Tim: Bekerja sama dengan tim pengembangan lainnya, termasuk Front End Developer, Backend Developer, dan Manajer Produk, untuk memastikan keselarasan antara desain dan implementasi teknis.</li>
+			<li>Memperbarui dan Memelihara: Mengikuti perkembangan tren dan teknologi terbaru dalam desain UI/UX, serta memperbarui dan memelihara desain produk secara kontinu untuk tetap relevan dan memenuhi kebutuhan pengguna yang terus berkembang.</li>
+          </ul>
+          <p>
+            UI/UX Developer harus memiliki pemahaman yang mendalam tentang prinsip desain UI/UX, pengetahuan tentang perilaku pengguna, kreativitas dalam 
+			merancang solusi yang inovatif, serta kemampuan untuk berkomunikasi dan berkolaborasi dengan baik dalam tim pengembangan.
+          </p>
+
+          <section>
+            <h3>Hard skill</h3>
+            <img src="images/hardskill.jpg" alt="hardskill" class="content-image" />
+            <p>
+              Kemampuan yang berkaitan dengan teknis dan bisa menunjang pekerjaan sebagai UI/UX designer. Deretan skill-nya meliputi:  <br />
+			  <li>Critical thinking kemampuan berpikir rasional untuk menilai apakah keputusan atau informasi tersebut logis dan sudah tepat</li>
+			  <li>Analytical thinking yaitu melihat segala hal dengan lebih rasional dan memudahkan dalam mengevaluasi serta menganalisis kebenaran suatu informasi</li>
+			  <li>Problem solving yakni kemampuan untuk menyelesaikan masalah juga dibutuhkan oleh UI/UX dalam proses menciptakan produk</li>
+			  <li>Product strategy, dalam membuat produk, UI/UX harus memiliki kemampuan membuat strategi. Dengan apa dan bagaimana cara sebuah produk dibuat</li>
+			  <li>Research, kemampuan riset yang baik sangat berguna untuk menunjang pekerjaan sebagai UI/UX karena Anda harus memahami kebutuhan dan ekspektasi user terhadap produk maupun layanan. Riset bisa dilalui dengan wawancara, survei, dan kuesioner. </li>
+			  <li>Desain, UI/UX memerlukan skill desain karena desain inilah yang digunakan untuk berkomunikasi dan menyampaikan pesan bagi customer. </li>
+            </p>
+          </section>
+
+          <section>
+            <h3>Softskill </h3>
+            <img src="images/softskill.jpg" alt="softskill" class="content-image" />
+            <p>
+             Keahlian di luar teknis juga dibutuhkan untuk menunjang pekerjaan diantaranya empati, komunikasi, kerja sama, empati, dan manajemen project. </p>
+			 <p>
+			 Kemampuan komunikasi yang baik UX/UI designer berguna untuk merepresentasikan blueprint dan prototype produk yang sudah dirancang.</p>
+			 <p>
+			 Sebagaimana diketahui, UI/UX tidak bisa bekerja sendiri karena melibatkan beberapa divisi lain. Oleh karena itu, communication skill dan teamwork sangat memudahkan proses kerja. </p>
+            </p>
+          </section>
+        </article>
+
+        <article class="card" id="cloud-computing">
+          <h2>Cloud Computing Developer</h2>
+          <img src="images/cloud-computing.jpg" alt="cloud-computing" class="content-image" />
+          <p>
+            Seorang Cloud Computing Developer adalah seorang profesional di bidang pengembangan perangkat lunak yang memiliki pengetahuan dan keahlian khusus dalam mengembangkan aplikasi dan infrastruktur menggunakan teknologi cloud computing.
+			<br>Tugas seorang Cloud Computing Developer meliputi: <br />
+          </p>
+          <ul>
+            <li>Desain dan Pengembangan Aplikasi Cloud: Merancang, mengembangkan, dan mengimplementasikan aplikasi yang dioptimalkan untuk dijalankan di lingkungan cloud. Ini meliputi pemilihan dan penggunaan layanan cloud yang sesuai, seperti penyimpanan data, pemrosesan, pengelolaan kontainer, dan skalabilitas.</li>
+            <li>Pemrograman dan Integrasi: Menggunakan bahasa pemrograman dan alat-alat pengembangan untuk mengimplementasikan solusi cloud, seperti menggunakan AWS (Amazon Web Services), Azure, Google Cloud Platform, atau platform cloud lainnya. Mereka juga bertanggung jawab untuk mengintegrasikan aplikasi dengan layanan cloud dan mengoptimalkan kinerja dan efisiensi.</li>
+            <li>Konfigurasi Infrastruktur Cloud: Merancang dan mengatur infrastruktur yang diperlukan untuk menjalankan aplikasi di lingkungan cloud, termasuk konfigurasi server, jaringan, dan pengaturan keamanan yang tepat. Hal ini melibatkan pengetahuan tentang konsep dan alat-alat yang digunakan dalam komputasi awan, seperti serverless computing, containerization, dan manajemen orkestrasi.</li>
+            <li>Keamanan dan Manajemen: Memastikan keamanan dan perlindungan data dalam lingkungan cloud dengan menerapkan praktik-praktik terbaik dalam keamanan informasi dan memastikan pemantauan dan manajemen yang efektif. Mereka juga bertanggung jawab untuk mengelola dan memantau kinerja aplikasi dan infrastruktur cloud.</li>
+			<li>Skalabilitas dan Ketersediaan: Mengembangkan aplikasi dan infrastruktur yang dapat berkembang secara horizontal (scale horizontally) atau vertikal (scale vertically) secara otomatis untuk mengatasi permintaan yang meningkat. Mereka juga berfokus pada ketersediaan aplikasi dan pemulihan bencana (disaster recovery) di lingkungan cloud.</li>
+			<li>Pemeliharaan dan Pembaruan: Memperbarui dan memelihara aplikasi dan infrastruktur cloud secara teratur, termasuk penerapan pembaruan keamanan, pemantauan kinerja, dan peningkatan fungsionalitas.</li>
+			<li>Kolaborasi Tim: Bekerja sama dengan tim pengembangan lainnya, seperti arsitek sistem, pengembang front-end dan back-end, serta profesional lain dalam pengembangan perangkat lunak, untuk memastikan integrasi yang baik antara aplikasi dan infrastruktur cloud.</li>
+          </ul>
+          <p>
+            Seorang Cloud Computing Developer harus memiliki pemahaman yang kuat tentang konsep dan teknologi cloud computing, pengetahuan tentang arsitektur aplikasi berbasis cloud, dan keterampilan pemrograman yang solid. Mereka juga harus dapat beradaptasi dengan cepat dengan perkembangan teknologi dan memiliki kemampuan analitis yang baik untuk memecahkan masalah dan mengoptimalkan kinerja aplikasi dan infrastruktur cloud.
+          </p>
+        </article>
+      </div>
+
+      <!-- side content  -->
+      <aside>
+        <article class="profile card">
+          <header>
+            <h2>Profile Saya</h2>
+            <figure>
+              <img src="images/foto-bio.jpg" src="foto bio" />
+            </figure>
+          </header>
+          <section>
+            <table>
+              <tr>
+                <th>Nama</th>
+                <td>: Ana Tsurayya Zakiya</td>
+              </tr>
+              <tr>
+                <th>Tempat Lahir</th>
+                <td>: Bengkulu</td>
+              </tr>
+              <tr>
+                <th>Tanggal Lahir</th>
+                <td>: 24 Maret 2002</td>
+              </tr>
+              <tr>
+                <th>Domisili</th>
+                <td>: Yogyakarta</td>
+              </tr>
+              <tr>
+                <th>Status</th>
+                <td>: Mahasiswa</td>
+              </tr>
+              <tr>
+                <th>Instansi</th>
+                <td>: Universitas Islam Negeri Sunan Kalijaga Yogyakarta</td>
+              </tr>
+              <tr>
+                <th>Email</th>
+                <td>: anatsurayya03@gmail.com</td>
+              </tr>
+              <tr>
+                <th>Linkedin</th>
+                <td>: <a href="https://www.linkedin.com/in/ana-tsurayya-zakiya-509369193/" target="_blank">Ana Tsurayya Zakiya</a></td>
+              </tr>
+              <tr>
+                <th>Instagram</th>
+                <td>: <a href="https://www.instagram.com/anatsurayyaz/" target="_blank">anatsurayyaz</a></td>
+              </tr>
+            </table>
+          </section>
+        </article>
+      </aside>
+    </main>
+
+    <!-- footer  -->
+    <footer>
+      <p>Ana Tsurayya Zakiya &copy; Dicoding 2023</p>
+    </footer>
+
+    <!-- script  -->
+    <script src="js/script.js"></script>
+  </body>
+</html>
